5e9caf465106c3d97d2b68f74592ef0558e700d9
andrewolee <andrewotsukalee@gmail.com>
2019-06-02 06:37:59 +0900

    refactor connect4

--- connect4.cpp
+++ connect4.cpp
@@ -1,214 +1,158 @@
 #include "minimax.h"
 #include <stdio.h>
 
+#define O_WIN INT_MIN
+#define X_WIN INT_MAX
+
 const int HEIGHT = 6;
 const int  WIDTH = 7;
 
+enum Chip {None, X, O};
+
 struct Board {
-	char state[HEIGHT][WIDTH];
+    Chip state[WIDTH][HEIGHT];
+    // The last move played, for easier win/loss checking
+    Chip chip;
+    int x;
+    int y;
 };
 
 class Connect4 : public Game<Board> {
 private:
-	Board board;
+    Board board;
 
 public:
-	Connect4 () {
-		for (int i = 0; i < HEIGHT; i++) {
-			for (int j = 0; j < WIDTH; j++) {
-				board.state[i][j] = '_';
-			}
-		}
-	}
+    Connect4 () {
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH; x++) {
+                board.state[x][y] = None;
+            }
+        }
+    }
 
-	void print_board() {
-		printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-		for (int i = 0; i < HEIGHT; i++) {
-			for (int j = 0; j < WIDTH; j++) {
-				printf("|%c", board.state[i][j]);
-			}
-			printf("|\n");
-		} 
-		for (int i = 0; i < WIDTH; i++) {
-			printf("|%i", i + 1);
-		}
-		printf("|\n");
-	}
+    void print_board() {
+        for (int i = 0; i < 10; i++) {
+            printf("\n");
+        }
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH; x++) {
+                printf("|");
+                switch (board.state[x][y]) {
+                    case None:
+                        printf("_");
+                        break;
+                    case X:
+                        printf("x");
+                        break;
+                    case O:
+                        printf("y");
+                }
+            }
+            printf("|\n");
+        } 
+        for (int x = 1; x < WIDTH + 1; x++) {
+            printf("|%i", x);
+        }
+        printf("|\n");
+    }
 
-	void player_move() {
-		int j = 0;
-		do {
-			printf("\nEnter column number to place chip: \n");
-			scanf("%u", &j);
-		} while(j < 1 || WIDTH < j);
-		j--;
-		int i = 0;
-		while (board.state[i][j] == '_' && i < HEIGHT){
-			i++;
-		}
-		board.state[i - 1][j] = 'o';
-	}
+    void player_move() {
+        int x = 0;
+        do {
+            printf("\nEnter column number to place chip: \n");
+            scanf("%u", &x);
+        } while(x < 1 || WIDTH < x);
+        x--;
+        int y = 0;
+        while (board.state[x][y] == None && y < HEIGHT){
+            y++;
+        }
+        y--;
+        board.state[x][y] = O;
+        board.x = x;
+        board.y = y;
+        board.chip = O;
+    }
 
-	void set_board(Board board) {
-		this->board = board;
-	}
+    void set_board(Board board) {
+        this->board = board;
+    }
 
-	Board get_board() {
-		return board;
-	}
+    Board get_board() {
+        return board;
+    }
 
-	std::vector<Board> legal_moves (Board board, bool maximize) {
-		std::vector<Board> boards;
-		if (evaluate(board) != 0) {
-			return boards;
-		}
-		for (int j = 0; j < WIDTH; j++) {
-			if (board.state[0][j] == '_') {
-				int i = 0;
-				while (board.state[i][j] == '_' && i < HEIGHT){
-					i++;
-				}
-				Board cpy = board;
-				if (maximize) {
-					cpy.state[i - 1][j] = 'x';
-				} else {
-					cpy.state[i - 1][j] = 'o';
-				}
-				boards.push_back(cpy);
-			}
-		}
-		return boards;
-	}
+    std::vector<Board> legal_moves (Board board, bool maximize) {
+        std::vector<Board> boards;
+        if (evaluate(board) != 0) {
+            return boards;
+        }
+        for (int x = 0; x < WIDTH; x++) {
+            if (board.state[x][0] == None) {
+                int y = 0;
+                while (board.state[x][y] == None && y < HEIGHT){
+                    y++;
+                }
+                y--;
+                Board cpy = board;
+                Chip chip = maximize ? X : O;
+                cpy.state[x][y] = chip;
+                cpy.chip = chip;
+                cpy.x = x;
+                cpy.y = y;
+                boards.push_back(cpy);
+            }
+        }
+        return boards;
+    }
 
-	int evaluate (Board board) {
-		int count = 0;
-		char player = '_';
-		// Check for vertical wins
-		for (int j = 0; j < WIDTH; j++) {
-			for (int i = 0; i < HEIGHT; i++) {
-				if (board.state[i][j] == player && player != '_') {
-					count++;
-					if (count == 4) {
-						if (player == 'x') {
-							return INT_MAX;
-						} else {
-							return INT_MIN;
-						}
-					}
-				} else {
-					player = board.state[i][j];
-					count = 1;
-				}
-			}
-		}
-
-		count = 0;
-		player = '_';
-		// Check for horizontal wins
-		for (int i = 0; i < HEIGHT; i++) {
-			for (int j = 0; j < WIDTH; j++) {
-				if (board.state[i][j] == player && player != '_') {
-					count++;
-					if (count == 4) {
-						if (player == 'x') {
-							return INT_MAX;
-						} else {
-							return INT_MIN;
-						}
-					}
-				} else {
-					player = board.state[i][j];
-					count = 1;
-				}
-			}
-		}
-
-		count = 0; 
-		player = '_';
-		// Check for diagonal wins
-		for (int i = 0; i < HEIGHT; i++) {
-			for (int k = 0; k < WIDTH && i + k < HEIGHT; k++) {
-				if (board.state[i + k][k] == player && player != '_') {
-					count++;
-					if (count == 4) {
-						if (player == 'x') {
-							return INT_MAX;
-						} else {
-							return INT_MIN;
-						}
-					}
-				} else {
-					player = board.state[i + k][k];
-					count = 1;
-				}
-			}
-		}
-
-		count = 0; 
-		player = '_';
-		// Check for diagonal wins
-		for (int i = 0; i < HEIGHT; i++) {
-			for (int k = 0; k < WIDTH && 0 <= i - k; k++) {
-				if (board.state[i - k][k] == player && player != '_') {
-					count++;
-					if (count == 4) {
-						if (player == 'x') {
-							return INT_MAX;
-						} else {
-							return INT_MIN;
-						}
-					}
-				} else {
-					player = board.state[i - k][k];
-					count = 1;
-				}
-			}
-		}
-
-		count = 0; 
-		player = '_';
-		// Check for diagonal wins
-		for (int i = 0; i < HEIGHT; i++) {
-			for (int k = 0; k < WIDTH && 0 <= i - k; k++) {
-				if (board.state[i - k][k] == player && player != '_') {
-					count++;
-					if (count == 4) {
-						if (player == 'x') {
-							return INT_MAX;
-						} else {
-							return INT_MIN;
-						}
-					}
-				} else {
-					player = board.state[i - k][k];
-					count = 1;
-				}
-			}
-		}
-
-		return 0;
-	}
+    int evaluate (Board board) {
+        // Vertical wins
+        int count = 0;
+        for (int y = std::max(0, board.y - 4); y < std::min(HEIGHT, board.y + 4); y++) {
+            if (board.state[board.x][y] == board.chip) {
+                count++;
+            } else {
+                count = 0;
+            }
+            if (count == 4) {
+                return board.chip == O ? O_WIN : X_WIN;
+            }
+        }
+        // Horizontal wins
+        count = 0;
+        for (int x = std::max(0, board.x - 4); x < std::min(HEIGHT, board.x + 4); x++) {
+            if (board.state[x][board.y] == board.chip) {
+                count++;
+            } else {
+                count = 0;
+            }
+            if (count == 4) {
+                return board.chip == O ? O_WIN : X_WIN;
+            }
+        }
+        // Diagonal wins
+        return 0;
+    }
 };
 
 int main(void) {
-	Connect4* connect4 = new Connect4();
-	Minimax<Board> minimax = Minimax<Board>(connect4, 6);
-	while (true) {
-		connect4->print_board();
-		connect4->player_move();
-		if (connect4->evaluate(connect4->get_board()) == INT_MIN) {
-			connect4->print_board();
-			printf("\nYou win!\n\n:)\n\n");
-			break;
-		} 
-		connect4->set_board(minimax.best_move(connect4->get_board(), true));
-		if (connect4->evaluate(connect4->get_board()) == INT_MAX) {
-			connect4->print_board();
-			printf("\nYou lose!\n\n:(\n\n");
-			break;
-		} 
-	}
+    Connect4* connect4 = new Connect4();
+    Minimax<Board> minimax = Minimax<Board>(connect4, 8);
+    while (true) {
+        connect4->print_board();
+        connect4->player_move();
+        if (connect4->evaluate(connect4->get_board()) == O_WIN) {
+            connect4->print_board();
+            printf("\nYou win!\n\n:)\n\n");
+            break;
+        } 
+        connect4->set_board(minimax.best_move(connect4->get_board(), true));
+        if (connect4->evaluate(connect4->get_board()) == X_WIN) {
+            connect4->print_board();
+            printf("\nYou lose!\n\n:(\n\n");
+            break;
+        } 
+    }
 }
-
-
 
