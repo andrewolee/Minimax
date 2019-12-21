0cd12a4618e5781283ca8648026df94280505491
andrewolee <andrewotsukalee@gmail.com>
2019-12-20 16:09:51 -0800

    fixed spacing

--- minimax.h
+++ minimax.h
@@ -11,13 +11,13 @@ public:
         for an advantage to the maximizer and a negative integer for an 
         advantage to the minimizer
     */
-    virtual int evaluate (T state) = 0;
+    virtual int evaluate(T state) = 0;
 
     /*
         Vector of all legal moves from 'state'. In a win/lose state, 
         'legal_moves' should return an empty vector.
     */
-    virtual std::vector<T> legal_moves (T state, bool maximize) = 0;
+    virtual std::vector<T> legal_moves(T state, bool maximize) = 0;
 };
 
 template <class T>
@@ -28,7 +28,7 @@ private:
     std::default_random_engine rng; // For returning random best move when there are multiple best moves
 
     // Actual minimax. Returns the score of a state
-    int minimax (T state, bool maximize, int depth, int alpha, int beta) {
+    int minimax(T state, bool maximize, int depth, int alpha, int beta) {
         std::vector<T> moves = game->legal_moves(state, maximize);
         if (depth == strength || moves.empty()) {
             // Wins/losses in the far future are scored lower
@@ -53,13 +53,13 @@ private:
 
 public:
     // Time based seed
-    Minimax (Game<T> *game, int strength) : game(game), strength(strength) {
+    Minimax(Game<T> *game, int strength) : game(game), strength(strength) {
         int seed = std::chrono::system_clock::now().time_since_epoch().count();
         rng = std::default_random_engine(seed);
     }
 
     // Minimax interface
-    T best_move (T state, bool maximize) {
+    T best_move(T state, bool maximize) {
         std::vector<T> moves = game->legal_moves(state, maximize);
         std::shuffle(moves.begin(), moves.end(), rng);
         int score = maximize ? INT_MIN : INT_MAX;
